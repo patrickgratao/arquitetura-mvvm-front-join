21b9bff520b97c0df8079e85c05e5174c3e84fbf
Patrick Gratão <ppgratao@gmail.com>
2025-09-20 01:00:57 -0300

    feat: add styles to posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+// import { PostsList } from './Posts';
 import { UsersList } from './Users';
 
 export default function App() {
   return (
     <div className="app-container">
+      {/* <PostsList /> */}
       <UsersList />
     </div>
   );
